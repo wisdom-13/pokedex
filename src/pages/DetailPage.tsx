25e02e7ffc0f2fc96702ab65894e271392504282
jhlee <wisdom_13_@naver.com>
2023-03-12 19:10:21 +0900

    Component design - Detail 1

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import PokemonInfo from '../components/PokemonInfo';
+import Tabs from '../components/Tabs';
 
 type Params = {
   id: string;
 }
 
+type Tab = 'about' | 'stats' | 'evolution';
+
 const DetailPage: React.FC = () => {
   const { id } = useParams<Params>();
+  const [selectedTab, setSelectedTab] = useState<Tab>('about');
+
+  const handleClick = (tab: Tab) => {
+    setSelectedTab(tab)
+  }
 
   return (
     <div>
-      DetailPage : {id}
+      <PokemonInfo id='1' />
+      <Tabs tab={selectedTab} onClick={handleClick} color={{ name: "red", url: "" }} />
     </div>
   );
 }
